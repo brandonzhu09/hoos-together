6090633b31c557c4de77bfe00a6d1ed780b17c44
shahryarkiani <shahryarkiani04@gmail.com>
2022-10-15 15:50:13 -0400

    Create phoneNumberEntry HTML Page Implement joinPage in views.py

--- gittogether/interface/views.py
+++ gittogether/interface/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
 def mainPage(request):
-    return 
+
+
+
+
+def joinPage(request):
+    if request.method == "POST":
+        phoneNumber = request.POST["phoneNumber"]
+        eventId = request.path
+    if request.method == "GET":
+        eventId = request.path
+
