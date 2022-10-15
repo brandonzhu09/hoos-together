5e6921912e4634d87d32fe244c8ea939fbcdcc6f
shahryarkiani <shahryarkiani04@gmail.com>
2022-10-15 16:25:40 -0400

    Add code to inject elements into html rendering

--- gittogether/interface/views.py
+++ gittogether/interface/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django import render
+from models import Event
 
 # Create your views here.
 
 def mainPage(request):
-
+    return 3
 
 
 
@@ -12,6 +12,11 @@ def joinPage(request):
     if request.method == "POST":
         phoneNumber = request.POST["phoneNumber"]
         eventId = request.path
+        curEvent = Event.objects.get(pk=eventId)
     if request.method == "GET":
         eventId = request.path
+        eventDesc = 3
+        eventName = "Hello"
+        return render(request, "templates/joinPage.html", {'eventDesc': eventDesc, 'eventName': eventName})
+
 
