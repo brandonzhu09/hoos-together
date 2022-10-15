33d06dca5f6a59c03d00129c12a2fa911c37f5da
unknown <minhaoh03@gmail.com>
2022-10-15 14:56:45 -0400

    django

---
+++ gittogether/interface/models.py
@@ -0,0 +1,9 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    eventCode = models.IntegerField(primary_key = True)
+    eventName = models.TextField(max_length=25, blank=False)
+    eventTime = models.DateTimeField(blank=False)
+    eventDesc = models.TextField(max_length=100)
