143eb7c9fe5368df2cc51f718c6ee2604c6fd1b2
unknown <minhaoh03@gmail.com>
2022-10-15 15:54:18 -0400

    frontpage

--- gittogether/gittogether/urls.py
+++ gittogether/gittogether/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from interface.views import helloWorld
+from interface.views import mainPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', helloWorld),
+    path('', mainPage),
 ]
--- gittogether/interface/views.py
+++ gittogether/interface/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+
 def mainPage(request):
-    return 
+    
+    return render(request, 'home.html')
